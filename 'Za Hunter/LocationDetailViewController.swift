879415100fc6ea5199acd0cdf1bb44367eebe3ba
Mariam Fuzail <mfuzail@bsd220.org>
2020-03-04 10:42:17 -0600

    added nav controller

---
+++ 'Za Hunter/LocationDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LocationDetailViewController.swift
+//  'Za Hunter
+//
+//  Created by Mariam Fuzail on 3/4/20.
+//  Copyright © 2020 Mariam Fuzail. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailViewController: Codable {
+
+}
